6be5ecee9d4029f60b1a910342738ba5790ea0a8
Louis Chatriot <louis.chatriot@gmail.com>
2012-12-14 19:44:48 +0100

    Added beautify to make editing a breeze

--- routes/docEdit.js
+++ routes/docEdit.js
@@ -6,6 +6,7 @@
 var config = require('../lib/config')
   , db = require('../lib/db')
   , ObjectID = require('mongodb').ObjectID
+  , beautify = require('../lib/js-beautify').js_beautify
   ;
 
 module.exports = function (req, res, next) {
@@ -23,7 +24,7 @@ module.exports = function (req, res, next) {
 
   collection = db.collection(req.params.collection);
   collection.findOne({ _id: new ObjectID(req.params.id) }, function (err, doc) {
-    values.doc = JSON.stringify(doc);
+    values.doc = beautify(JSON.stringify(doc));
 
     res.render('layout', { values: values
                          , partials: partials
