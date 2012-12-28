bd98e4de74bf45f7c1b8f054563e1dfb7a662077
Louis Chatriot <louis.chatriot@gmail.com>
2012-12-28 14:56:04 +0100

    oops

--- routes/docChange.js
+++ routes/docChange.js
@@ -19,7 +19,7 @@ module.exports = function (req, res, next) {
   try {
     newDoc = serialization.deserializeFromGUI(req.body.newData);
   } catch(e) {
-    return res.json(403, { message: 'Badly formatted data', e });
+    return res.json(403, { message: 'Badly formatted data', error: e });
   }
 
   if (newDoc._id.toString() !== req.params.id) {
