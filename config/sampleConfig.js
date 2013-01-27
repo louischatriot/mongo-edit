579bf69c15508469cf23ca3be6721dbe40dd282d
Louis Chatriot <louis.chatriot@gmail.com>
2013-01-27 11:56:29 +0100

    Reorganized config.js to make it clearer

---
+++ config/sampleConfig.js
@@ -0,0 +1,10 @@
+{
+  db: { host: 'localhost'
+      , port: 27017
+      , name: 'dev-db'
+      }
+, svPort: 2763
+, trustProxy: false
+, templatesDir: 'templates'
+, pagination: { resultsPerPage: 100, pagesAroundCurrent: 2 }
+}
